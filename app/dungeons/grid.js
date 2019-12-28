5b21f31d591ddbff6c0cf4e95cff8b5c655a10bf
AJ Lemos <wajlemos@gmail.com>
2019-12-27 22:22:14 -0800

    organize

--- app/dungeons/grid.js
+++ app/dungeons/grid.js
@@ -3,6 +3,8 @@ import { roll, rollArrayItem } from '../utility/roll';
 
 export const wallSize = 1;
 
+export const cellBlank = '.';
+
 const sides = {
     top   : 'top',
     right : 'right',
@@ -50,4 +52,81 @@ export const getStartingPoint = ({ gridWidth, gridHeight }, { roomWidth, roomHei
     }
 
     return [ x, y ];
+};
+
+const checkArea = (grid, { x, y, width, height }) => {
+    let minX = wallSize;
+    let minY = wallSize;
+    let maxX = grid.length - wallSize;
+    let maxY = grid[0].length - wallSize;
+
+    for (let xCord = x; xCord < (x + width); xCord++) {
+        for (let yCord = y; yCord < (y + height); yCord++) {
+            if (xCord < minX || xCord >= maxX) {
+                return false;
+            }
+
+            if (yCord < minY || yCord >= maxY) {
+                return false;
+            }
+
+            if (grid[xCord][yCord] !== cellBlank) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
+
+const isRoomCorner = ({ x, y, minX, minY, maxX, maxY }) => {
+    let minLeft   = minX + wallSize;
+    let minTop    = minY + wallSize;
+    let minBottom = maxY - wallSize;
+    let minRight  = maxX - wallSize;
+
+    let upperLeft  = x <= minLeft  && y <= minTop;
+    let upperRight = x >= minRight && y <= minTop;
+    let lowerRight = x >= minRight && y >= minBottom;
+    let lowerLeft  = x <= minLeft  && y >= minBottom;
+
+    return upperLeft || upperRight || lowerRight || lowerLeft;
+};
+
+export const getValidRoomCords = (grid, prevRoom, { roomWidth, roomHeight }) => {
+    let {
+        x: prevX,
+        y: prevY,
+        width: prevWidth,
+        height: prevHeight,
+    } = prevRoom;
+
+    let minX = prevX - roomWidth - wallSize;
+    let minY = prevY - roomHeight - wallSize;
+    let maxX = prevX + prevWidth + wallSize;
+    let maxY = prevY + prevHeight + wallSize;
+
+    let validCords = [];
+
+    for (let x = minX; x <= maxX; x++) {
+        for (let y = minY; y <= maxY; y++) {
+            if (isRoomCorner({ x, y, minX, minY, maxX, maxY })) {
+                continue;
+            }
+
+            let valid = checkArea(grid, {
+                x, y,
+                width: roomWidth,
+                height: roomHeight,
+            });
+
+            if (!valid) {
+                continue;
+            }
+
+            validCords.push([ x, y ]);
+        }
+    }
+
+    return validCords;
 };
--- app/dungeons/map.js
+++ app/dungeons/map.js
@@ -1,13 +1,18 @@
 
 import { createAttrs } from '../utility/html';
-import { dimensionRanges, customDimensions, roomTypeSizes } from '../rooms/dimensions';
-import { getStartingPoint, wallSize } from './grid';
+import { dimensionRanges, customDimensions } from '../rooms/dimensions';
 import { knobs } from '../knobs';
 import { roll, rollArrayItem } from '../utility/roll';
 
+import {
+    cellBlank,
+    getStartingPoint,
+    getValidRoomCords,
+    wallSize,
+} from './grid';
+
 const debug = false;
 
-const cellBlank = '.';
 const cellWall  = 'w';
 
 const cellPx     = 20;
@@ -20,84 +25,6 @@ const gridBackground  = '#efefef';
 const gridStrokeColor = '#cfcfcf';
 const roomBackground  = '#ffffff';
 const roomStrokeColor = '#555555';
-
-
-const checkArea = (grid, { x, y, width, height }) => {
-    let minX = wallSize;
-    let minY = wallSize;
-    let maxX = grid.length - wallSize;
-    let maxY = grid[0].length - wallSize;
-
-    for (let xCord = x; xCord < (x + width); xCord++) {
-        for (let yCord = y; yCord < (y + height); yCord++) {
-            if (xCord < minX || xCord >= maxX) {
-                return false;
-            }
-
-            if (yCord < minY || yCord >= maxY) {
-                return false;
-            }
-
-            if (grid[xCord][yCord] !== cellBlank) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-};
-
-const isCorner = ({ x, y, minX, minY, maxX, maxY }) => {
-    let minLeft   = minX + wallSize;
-    let minTop    = minY + wallSize;
-    let minBottom = maxY - wallSize;
-    let minRight  = maxX - wallSize;
-
-    let upperLeft  = x <= minLeft  && y <= minTop;
-    let upperRight = x >= minRight && y <= minTop;
-    let lowerRight = x >= minRight && y >= minBottom;
-    let lowerLeft  = x <= minLeft  && y >= minBottom;
-
-    return upperLeft || upperRight || lowerRight || lowerLeft;
-};
-
-const getValidRoomCords = (grid, prevRoom, { roomWidth, roomHeight }) => {
-    let {
-        x: prevX,
-        y: prevY,
-        width: prevWidth,
-        height: prevHeight,
-    } = prevRoom;
-
-    let minX = prevX - roomWidth - wallSize;
-    let minY = prevY - roomHeight - wallSize;
-    let maxX = prevX + prevWidth + wallSize;
-    let maxY = prevY + prevHeight + wallSize;
-
-    let validCords = [];
-
-    for (let x = minX; x <= maxX; x++) {
-        for (let y = minY; y <= maxY; y++) {
-            if (isCorner({ x, y, minX, minY, maxX, maxY })) {
-                continue;
-            }
-
-            let valid = checkArea(grid, {
-                x, y,
-                width: roomWidth,
-                height: roomHeight,
-            });
-
-            if (!valid) {
-                continue;
-            }
-
-            validCords.push([ x, y ]);
-        }
-    }
-
-    return validCords;
-};
 
 const drawText = (text, { x, y }) => {
     let attrs = createAttrs({
