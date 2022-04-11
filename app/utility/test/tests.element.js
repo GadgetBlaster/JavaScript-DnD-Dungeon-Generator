0b624e00e0762a6e91328287eb9180c4a699d70d
AJ Lemos <wajlemos@gmail.com>
2022-04-11 00:06:43 -0700

    touchups

--- app/utility/test/tests.element.js
+++ app/utility/test/tests.element.js
@@ -18,42 +18,42 @@ export default ({ assert, describe, it }) => {
     // -- Private Functions ----------------------------------------------------
 
     describe('createAttributes()', () => {
-        it('should return a string', () => {
+        it('returns a string', () => {
             assert(createAttributes({ class: 'css-class' })).isString();
         });
 
         describe('given nothing', () => {
-            it('should return an empty string', () => {
+            it('returns an empty string', () => {
                 assert(createAttributes()).equals('');
             });
         });
 
         describe('given an empty object', () => {
-            it('should return an empty string', () => {
+            it('returns an empty string', () => {
                 assert(createAttributes({})).equals('');
             });
         });
 
         describe('given an object with a single key value pair', () => {
-            it('should return the key and value formatted as an HTML attribute string', () => {
+            it('returns the key and value formatted as an HTML attribute string', () => {
                 assert(createAttributes({ role: 'presentation' })).equals(' role="presentation"');
             });
         });
 
         describe('given an object with a boolean value', () => {
-            it('should format the value as a string', () => {
+            it('formats the value as a string', () => {
                 assert(createAttributes({ hidden: true })).equals(' hidden="true"');
             });
         });
 
         describe('given an object with a numeric value', () => {
-            it('should format the value as a string', () => {
+            it('formats the value as a string', () => {
                 assert(createAttributes({ 'data-columns': 2 })).equals(' data-columns="2"');
             });
         });
 
         describe('given an object with multiple key value pairs', () => {
-            it('should return the key and value pairs formatted as an HTML attribute string', () => {
+            it('returns the key and value pairs formatted as an HTML attribute string', () => {
                 const object = { 'data-target': 'kitten', id: 'jet-pack', type: 'submit' };
                 assert(createAttributes(object))
                     .equals(' data-target="kitten" id="jet-pack" type="submit"');
@@ -64,36 +64,36 @@ export default ({ assert, describe, it }) => {
     // -- Public Functions -----------------------------------------------------
 
     describe('element()', () => {
-        it('should return a string', () => {
+        it('returns a string', () => {
             assert(element('p')).isString();
         });
 
-        describe('given the tag `section`', () => {
-            it('should return a `section` html element string', () => {
+        describe('given the tag "section"', () => {
+            it('returns a `<section>` html element string', () => {
                 assert(element('section')).equals('<section></section>');
             });
         });
 
-        describe('given a non-empty `content` string', () => {
-            it('should render the content string inside the element string', () => {
+        describe('given a non-empty content string', () => {
+            it('renders the content inside the element string', () => {
                 assert(element('h1', 'Howdy Partner')).equals('<h1>Howdy Partner</h1>');
             });
         });
 
-        describe('given an `attrs` object', () => {
-            it('should render the element string with correct attributes', () => {
+        describe('given an attributes object', () => {
+            it('renders the element string with correct attributes', () => {
                 const el = element('div', 'Always out numbered', { 'data-type': 'ranger' });
                 assert(el).equals('<div data-type="ranger">Always out numbered</div>');
             });
         });
 
         describe('given a self closing tag', () => {
-            it('should return a self closing html element string', () => {
+            it('returns a self closing html element string', () => {
                 assert(element('input')).equals('<input />');
             });
 
-            describe('given an `attrs` object', () => {
-                it('should render the element string with correct attributes', () => {
+            describe('given an attributes object', () => {
+                it('renders the element string with correct attributes', () => {
                     const el = element('img', null, {
                         'alt': 'Dragons of Twilight',
                         'src': 'dragons.jpg',
@@ -104,7 +104,7 @@ export default ({ assert, describe, it }) => {
             });
 
             describe('given content', () => {
-                it('should throw', () => {
+                it('throws', () => {
                     assert(() => element('img', 'I do not belong here'))
                         .throws('Content is not allowed in self closing elements');
                 });
--- app/utility/test/tests.tools.js
+++ app/utility/test/tests.tools.js
@@ -26,7 +26,7 @@ export default ({ assert, describe, it }) => {
 
     describe('capitalize()', () => {
         describe('given a string that starts with a lowercase letter', () => {
-            it('should capitalize the first letter in the string', () => {
+            it('capitalizes the first letter in the string', () => {
                 assert(capitalize('joe bob')).equals('Joe bob');
             });
         });
@@ -34,14 +34,14 @@ export default ({ assert, describe, it }) => {
 
     describe('indefiniteArticle()', () => {
         describe('given a word that does not start with a vowel', () => {
-            it('should return "a"', () => {
+            it('returns "a"', () => {
                 assert(indefiniteArticle('hammock')).equals('a');
             });
         });
 
         [ 'apple', 'elephant', 'igloo', 'otter', 'udder' ].forEach((word) => {
-            describe('given a word that starts with a vowel', () => {
-                it('should return "an"', () => {
+            describe(`given "${word}", which starts with a vowel`, () => {
+                it('returns "an"', () => {
                     assert(indefiniteArticle(word)).equals('an');
                 });
             });
@@ -50,64 +50,66 @@ export default ({ assert, describe, it }) => {
 
     describe('listSentence()', () => {
         describe('given no items', () => {
-            it('should return undefined', () => {
+            it('returns undefined', () => {
                 assert(listSentence([])).isUndefined();
             });
         });
 
         describe('given an array of one strings', () => {
-            it('should return the string', () => {
+            it('returns the string', () => {
                 assert(listSentence([ 'primus' ])).equals('primus');
             });
         });
 
         describe('given an array of two strings', () => {
-            it('should return the strings joined by `and` with first word capitalized', () => {
-                assert(listSentence([ 'hummingbirds', 'jellyfish' ])).equals('hummingbirds and jellyfish');
+            it('returns the strings joined by "and"', () => {
+                assert(listSentence([ 'hummingbirds', 'jellyfish' ]))
+                    .equals('hummingbirds and jellyfish');
             });
         });
 
         describe('given an array of three strings', () => {
-            it('should return the strings joined by commas with `and` between the last two', () => {
+            it('returns the strings joined by commas with "and" between the last two', () => {
                 assert(listSentence([ 'hammers', 'polar bears', 'walruses' ]))
                     .equals('hammers, polar bears, and walruses');
             });
         });
 
         describe('given an array of four strings', () => {
-            it('should return the strings joined by commas with `and` between the last two', () => {
-                assert(listSentence([ 'one', 'two', 'three', 'four' ])).equals('one, two, three, and four');
+            it('returns the strings joined by commas with "and" between the last two', () => {
+                assert(listSentence([ 'one', 'two', 'three', 'four' ]))
+                    .equals('one, two, three, and four');
             });
         });
     });
 
     describe('pluralize()', () => {
-        describe('given a count of 1', () => {
-            it('should return the string', () => {
-                assert(pluralize(1, 'cat')).equals('cat');
-            });
-        });
-
         describe('given a count of 0', () => {
-            it('should return a pluralized string', () => {
+            it('pluralizes the string', () => {
                 assert(pluralize(0, 'cat')).equals('cats');
             });
         });
 
+        describe('given a count of 1', () => {
+            it('returns the string unmodified', () => {
+                assert(pluralize(1, 'cat')).equals('cat');
+            });
+        });
+
         describe('given a count of 2', () => {
-            it('should return a pluralized string', () => {
+            it('pluralizes the string', () => {
                 assert(pluralize(2, 'cat')).equals('cats');
             });
         });
 
         describe('given a count of 3', () => {
-            it('should return the string', () => {
+            it('pluralizes the string', () => {
                 assert(pluralize(3, 'cat')).equals('cats');
             });
         });
 
         describe('given a count of 2 and a suffix of `es`', () => {
-            it('should return the string', () => {
+            it('pluralizes the string', () => {
                 assert(pluralize(2, 'walrus', 'es')).equals('walruses');
             });
         });
@@ -115,16 +117,18 @@ export default ({ assert, describe, it }) => {
 
     describe('toDash()', () => {
         describe('given a string with spaces', () => {
-            it('should return a lower cased string with the words dash separated', () => {
-                assert(toDash('Teleporting Mythical Creature')).equals('teleporting-mythical-creature');
+            it('returns a lower cased string with the words dash separated', () => {
+                assert(toDash('Teleporting Mythical Creature'))
+                    .equals('teleporting-mythical-creature');
             });
         });
     });
 
     describe('toWords()', () => {
         describe('given a camel case string', () => {
-            it('should return a lower cased string broken into multiple words', () => {
-                assert(toWords('bustedBulldozingBrachiosaurus')).equals('busted bulldozing brachiosaurus');
+            it('returns a lower cased string broken into multiple words', () => {
+                assert(toWords('bustedBulldozingBrachiosaurus'))
+                    .equals('busted bulldozing brachiosaurus');
             });
         });
     });
@@ -133,19 +137,19 @@ export default ({ assert, describe, it }) => {
 
     describe('isEven()', () => {
         describe('given an odd number', () => {
-            it('should return `false`', () => {
+            it('returns `false`', () => {
                 assert(isEven(3)).isFalse();
             });
         });
 
         describe('given an even number', () => {
-            it('should return `true`', () => {
+            it('returns `true`', () => {
                 assert(isEven(12)).isTrue();
             });
         });
 
         describe('given zero', () => {
-            it('should return `true`', () => {
+            it('returns `true`', () => {
                 assert(isEven(0)).isTrue();
             });
         });
@@ -153,19 +157,19 @@ export default ({ assert, describe, it }) => {
 
     describe('isOdd()', () => {
         describe('given an odd number', () => {
-            it('should return `true`', () => {
+            it('returns `true`', () => {
                 assert(isOdd(1)).isTrue();
             });
         });
 
         describe('given an even number', () => {
-            it('should return `true`', () => {
+            it('returns `true`', () => {
                 assert(isOdd(6)).isFalse();
             });
         });
 
         describe('given zero', () => {
-            it('should return `false`', () => {
+            it('returns `false`', () => {
                 assert(isOdd(0)).isFalse();
             });
         });
@@ -175,8 +179,9 @@ export default ({ assert, describe, it }) => {
 
     describe('chunk()', () => {
         describe('given an empty array', () => {
-            it('should return an empty array', () => {
+            it('returns an empty array', () => {
                 let chunks = chunk([], 0);
+
                 assert(chunks).isArray();
                 assert(chunks.length).equals(0);
             });
@@ -185,27 +190,22 @@ export default ({ assert, describe, it }) => {
         describe('given an array of 10 items and chunk size of 2', () => {
             let chunks = chunk(Array.from(Array(10).keys()), 2);
 
-            it('should return an array', () => {
+            it('returns an array', () => {
                 assert(chunks).isArray();
             });
 
-            it('should return an array with five items', () => {
+            it('returns an array with five items', () => {
                 assert(chunks.length).equals(5);
             });
 
-            it('should return an array', () => {
+            it('each item is an array with two items', () => {
                 chunks.forEach((c) => {
                     assert(c).isArray();
-                });
-            });
-
-            it('should have tow items in each array', () => {
-                chunks.forEach((c) => {
                     assert(c.length).equals(2);
                 });
             });
 
-            it('should group the items in groups of two in sequential order', () => {
+            it('groups the items in sequential order', () => {
                 assert(chunks[0]).equalsArray([ 0, 1 ]);
                 assert(chunks[1]).equalsArray([ 2, 3 ]);
                 assert(chunks[2]).equalsArray([ 4, 5 ]);
@@ -214,39 +214,39 @@ export default ({ assert, describe, it }) => {
             });
         });
 
-        describe('given an array of 4 and a chunk size of 3', () => {
-            it('should have the remainder in the last array', () => {
+        describe('given an array of 4 items and a chunk size of 3', () => {
+            it('puts the remainder in the last array', () => {
                 let chunks = chunk([ 1, 2, 3, 4 ], 3);
+
                 assert(chunks[1].length).equals(1);
                 assert(chunks[1].pop()).equals(4);
             });
         });
 
         describe('given a chunk size the same as the array length', () => {
-            it('should have a single chunk', () => {
-                let chunks = chunk(Array.from(Array(3)), 3);
+            let chunks = chunk(Array.from(Array(3).keys()), 3);
+
+            it('has a single chunk', () => {
                 assert(chunks.length).equals(1);
             });
 
-            it('should have a single chunk the same length as the array', () => {
-                let chunks = chunk(Array.from(Array(3)), 3);
+            it('has a single chunk the same length as the array', () => {
                 assert(chunks[0].length).equals(3);
             });
 
-            it('should have a chunk identical to the array', () => {
-                let chunks = chunk([ 0, 1, 2 ], 3);
+            it('has a chunk identical to the array', () => {
                 assert(chunks[0]).equalsArray([ 0, 1, 2 ]);
             });
         });
 
         describe('given a chunk size larger than the array length', () => {
-            it('should have a single chunk', () => {
-                let chunks = chunk(Array.from(Array(3).keys()), 4);
+            let chunks = chunk(Array.from(Array(3).keys()), 4);
+
+            it('has a single chunk', () => {
                 assert(chunks.length).equals(1);
             });
 
-            it('should have a chunk identical to the array', () => {
-                let chunks = chunk(Array.from(Array(3).keys()), 4);
+            it('has a chunk identical to the array', () => {
                 assert(chunks[0]).equalsArray([ 0, 1, 2 ]);
             });
         });
@@ -278,21 +278,24 @@ export default ({ assert, describe, it }) => {
         });
 
         describe('given a minimums object with a single minimum', () => {
-            it('should return a range lookup keyed the same as the minimums object', () => {
+            it('returns a range lookup keyed the same as the minimums object', () => {
                 assert(createRangeLookup({ gopher: 23 }))
-                    .equalsObject({ gopher: { min: 23, max: Number.POSITIVE_INFINITY }});
+                    .equalsObject({ gopher: {
+                        min: 23,
+                        max: Number.POSITIVE_INFINITY,
+                    }});
             });
         });
 
         describe('given a minimums object with a maximum value', () => {
-            it('should return a range lookup with the correct maximum', () => {
+            it('returns a range lookup with the correct maximum', () => {
                 assert(createRangeLookup({ gopher: 23 }, 32))
                     .equalsObject({ gopher: { min: 23, max: 32 }});
             });
         });
 
         describe('given a minimums object with several values', () => {
-            it('should return a range lookup with correct ranges', () => {
+            it('returns a range lookup with correct ranges', () => {
                 assert(createRangeLookup({
                     gopher: 23,
                     bird  : 24,
@@ -314,7 +317,7 @@ export default ({ assert, describe, it }) => {
     // -- Throw ----------------------------------------------------------------
 
     describe('toss()', () => {
-        it('should throw a type error', () => {
+        it('throws', () => {
             assert(() => toss('These aren\'t the droids you are looking for.'))
                 .throws('These aren\'t the droids you are looking for.');
         });
@@ -322,13 +325,13 @@ export default ({ assert, describe, it }) => {
 
     describe('isRequired()', () => {
         describe('given an undefined value', () => {
-            it('should throw the given message', () => {
+            it('throws the given message', () => {
                 assert(() => isRequired(undefined, 'No good dude')).throws('No good dude');
             });
         });
 
         describe('given defined value', () => {
-            it('should return undefined', () => {
+            it('returns undefined', () => {
                 assert(isRequired('Good stuff', 'No good dude')).equals(undefined);
             });
         });
